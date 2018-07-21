ad82fedae01265025849454393d4f65e784e0a7f
wk-j <somnuk.wk@gmail.com>
2018-07-21 18:48:40 +0700

    Load workspace

--- src/DotNetUpdate/Program.cs
+++ src/DotNetUpdate/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Buildalyzer;
+using Buildalyzer.Workspaces;
 
-namespace DotNetUpdate
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace DotNetUpdate {
+    class Program {
+        static void Main(string[] args) {
+            var project = @"/Users/wk/Source/DotNetUpdate/tests/MyApp/MyApp.csproj";
+            var manager = new AnalyzerManager();
+            var analyzer = manager.GetProject(project);
+            var sourcesFiles = analyzer.GetSourceFiles();
+            var references = analyzer.GetReferences();
+
+            foreach (var file in sourcesFiles) {
+                Console.WriteLine(file);
+            }
+
+            foreach (var item in references) {
+                Console.WriteLine(item);
+            }
+            var workspace = analyzer.GetWorkspace();
         }
     }
 }
---
+++ tests/MyApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
